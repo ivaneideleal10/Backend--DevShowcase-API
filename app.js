23e24d79a05531eae4b5ec20ca654b8c4c6e5889
ivaneideleal10 <ivaneideleal10@outlook.com>
2026-09-20 16:57:58 -0300

    Configuração inicial do projeto

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        mensagem: "DevShowcase API funcionando!"
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000");
+});
